aa0b0eef4618c62bd0e6579d91c28439648198f4
guimaya <148132668+guimaya@users.noreply.github.com>
2024-01-09 16:03:51 -0300

    idk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <p>Oiii</p>
       <iframe
         title='Embedded Report'
         width='1280'
         height='720'
         src='https://lookerstudio.google.com/embed/reporting/5926df24-e27e-43e3-ba25-e69f6de326e0/page/6zXD'
-        frameBorder='0'
         style={{ border: 0 }}
         allowFullScreen
         sandbox='allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox'
